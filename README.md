795db61c9245810d896727d8979e8e1c2fa6df02
Rostyk Chaikivskyi <r.chaikivskyi@gmail.com>
2023-04-18 18:35:51 +0300

    source code URL

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# [Demo URL](https://react-shop.rostyk.dev/)
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,25 @@
 export default function Footer() {
   return (
     <footer className="bg-color-secondary py-8">
-      <div className="container">
+      <div className="container flex items-center justify-between">
         <small className="flex items-center text-white">
           Made with React
           <span className="material-symbols-outlined ml-1">code</span>
         </small>
+
+        <a
+          href="https://github.com/Rostyk27/react-shop/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white hover:shadow-[0_2px_0_0_rgba(255,255,255,1)]"
+        >
+          <small className="flex items-center">
+            Source code
+            <span className="material-symbols-outlined ml-1 text-[22px]">
+              deployed_code
+            </span>
+          </small>
+        </a>
       </div>
     </footer>
   );
